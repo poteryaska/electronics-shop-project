870e505833a0432e7fc5ddfc5aafead26f755df9
evgeniia lemanova <jane87.05@mail.ru>
2023-03-14 16:49:00 +0300

    add classmethod

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -16,17 +18,8 @@ class Item:
         self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        self.all.append(self)
 
-    @property
-    def name(self):
-        return self.__name
-    @name.setter
-    def name(self, value: str):
-        if len(value) > 10:
-            self.__name = value
-        else:
-            raise ValueError
 
     def calculate_total_price(self) -> float:
         """
@@ -42,3 +35,32 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, value: str):
+        if len(value) > 10:
+            raise Exception('Длина наименования товара превышает 10 символов')
+        self.__name = value
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        '''инициализирует экземпляры класса Item из файла src/items.csv'''
+        csv_data = []
+        with open('../src/items.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Item.__init__(row['name'], row['price'], row['quantity'])
+        #         csv_data.append(row['name'])
+        #         csv_data.append(row['price'])
+        # print(csv_data)
+        #         print(row)
+            #     csv_data.append(row['name'], row['price'], row['quantity'])
+
+
+    @staticmethod
+    def string_to_number(number: str):
+        return int(number)
